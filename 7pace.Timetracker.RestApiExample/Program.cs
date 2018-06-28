b94753c213f55e82396759c64c29610259600f15
Eugene Kolomytsev <eugene@7pace.com>
2018-06-28 16:50:51 +0300

    full cycle of test application: creating, updating, deleting worklog

--- 7pace.Timetracker.RestApiExample/Program.cs
+++ 7pace.Timetracker.RestApiExample/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
@@ -47,6 +49,20 @@ namespace _7pace.Timetracker.RestApiExample
                 { "$count", "10" }
             } );
 
+            var newWorkLog = await VerbAndPrint<MinimalWorkLog>( HttpMethod.Post, apiWorkLogsEndpoint, new Dictionary<string, string>()
+            {
+                { "length", "3600" },
+                { "comment", "test created" }
+            } );
+
+            var updatedWorklog = await VerbAndPrint<MinimalWorkLog>( new HttpMethod( "PATCH" ), new[] { apiWorkLogsEndpoint, newWorkLog.Data.Id.ToString() }, new Dictionary<string, string>()
+            {
+                { "length", "7200" },
+                { "comment", "test updated" }
+            } );
+
+            await VerbAndPrint<MinimalWorkLog>( HttpMethod.Delete, new[] { apiWorkLogsEndpoint, updatedWorklog.Data.Id.ToString() } );
+
             Console.WriteLine( "Finished. Press any key to close" );
             Console.ReadKey();
         }
@@ -59,10 +75,16 @@ namespace _7pace.Timetracker.RestApiExample
                    .WithOAuthBearerToken( Configuration[authTokenConfigName] );
         }
 
-        private static async Task QueryGetAndPrint ( string path, Dictionary<string, string> queryStringParameters = null )
+        private static Task QueryGetAndPrint ( string path, Dictionary<string, string> queryStringParameters = null )
         {
-            Console.WriteLine( $"Query /{path}" );
-            var queryRequest = GetBase().AppendPathSegment( path );
+            return QueryGetAndPrint( new[] { path }, queryStringParameters );
+        }
+
+        private static async Task QueryGetAndPrint ( string[] paths, Dictionary<string, string> queryStringParameters = null )
+        {
+            Console.WriteLine( $"Query GET /{string.Join( "/", paths )}" );
+            var queryRequest = GetBase();
+            queryRequest = paths.Aggregate( queryRequest, ( current, s ) => current.AppendPathSegment( s ) );
             if ( queryStringParameters != null )
             {
                 foreach ( KeyValuePair<string, string> queryStringParameter in queryStringParameters )
@@ -77,5 +99,55 @@ namespace _7pace.Timetracker.RestApiExample
             Console.WriteLine( "\r\nPress any key to continue\r\n" );
             Console.ReadKey();
         }
+
+        private static Task<SuccessResponse<T>> VerbAndPrint<T> ( HttpMethod verb, string path, Dictionary<string, string> bodyParameters = null )
+        {
+            return VerbAndPrint<T>( verb, new[] { path }, bodyParameters );
+        }
+
+        private static async Task<SuccessResponse<T>> VerbAndPrint<T> ( HttpMethod verb, string[] paths, Dictionary<string, string> bodyParameters = null )
+        {
+            Console.WriteLine( $"Query {verb} /{string.Join( "/", paths )}" );
+            var queryRequest = GetBase();
+            queryRequest = paths.Aggregate( queryRequest, ( current, s ) => current.AppendPathSegment( s ) );
+
+            HttpResponseMessage queryResult = null;
+            SuccessResponse<T> result = null;
+
+            if ( verb == HttpMethod.Post )
+            {
+                queryResult = await queryRequest.PostJsonAsync( bodyParameters );
+            }
+            else if ( verb == new HttpMethod( "PATCH" ) )
+            {
+                queryResult = await queryRequest.PatchJsonAsync( bodyParameters );
+            }
+            else if ( verb == HttpMethod.Delete )
+            {
+                queryResult = await queryRequest.DeleteAsync();
+            }
+            //todo put is not implemented
+
+            if ( queryResult != null )
+            {
+                var responseString = await queryResult.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<SuccessResponse<T>>( responseString );
+                Console.WriteLine( JsonConvert.DeserializeObject( responseString ) );
+            }
+
+            Console.WriteLine( "\r\nPress any key to continue\r\n" );
+            Console.ReadKey();
+            return result;
+        }
+    }
+
+    public class SuccessResponse<T>
+    {
+        public T Data { get; set; }
+    }
+
+    public class MinimalWorkLog
+    {
+        public Guid Id { get; set; }
     }
 }
